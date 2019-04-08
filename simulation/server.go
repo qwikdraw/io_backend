18180cec2f32088a83162b5ab52caa379ce9a10f
Nico <nicocollie@gmail.com>
2019-04-07 19:20:40 -0700

    Changed import path for protobuffer
    And some auto spacing from vscode / golang plug-in

--- simulation/server.go
+++ simulation/server.go
@@ -1,20 +1,21 @@
 package main
 
 import (
+	"encoding/binary"
 	"flag"
+	"fmt"
+	gp "io_backend/simulation/game_protocol"
 	"log"
 	"net/http"
-	ws "github.com/gorilla/websocket"
-	"fmt"
-	gp "server/game_protocol"
+
 	proto "github.com/golang/protobuf/proto"
-	"encoding/binary"
+	ws "github.com/gorilla/websocket"
 )
 
 var addr = flag.String("addr", "0.0.0.0:3000", "http service address")
 
 var upgrader = ws.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {return true},
+	CheckOrigin:  func(r *http.Request) bool { return true },
 	Subprotocols: []string{"binary"},
 }
 
@@ -27,16 +28,16 @@ func binary_response(w http.ResponseWriter, r *http.Request) {
 	defer c.Close()
 
 	test_entity := &gp.Entity{
-		Id: 23,
+		Id:         23,
 		Renderable: []uint32{42, 56},
 	}
 	update := &gp.Update{
-		Time: 634.0,
+		Time:   634.0,
 		Update: []*gp.Entity{test_entity},
 		Remove: []uint32{1, 2, 3},
 	}
 	out, _ := proto.Marshal(update)
-	packet := make([]byte, 2, len(out) + 2)
+	packet := make([]byte, 2, len(out)+2)
 	binary.LittleEndian.PutUint16(packet, uint16(len(out)))
 	packet = append(packet, out...)
 
