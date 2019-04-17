a70f5520b6ff6a6fc9b988e27c0a8a278937fb58
Logan James Kaser <logankaser@fyrn.io>
2019-04-17 16:54:11 -0700

    New protocol and improved websocket code

--- simulation/server.go
+++ simulation/server.go
@@ -13,7 +13,20 @@ import (
 	"time"
 )
 
-type Player struct {
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+)
+
+
+type Client struct {
 	world *World
 
 	// The websocket connection.
@@ -27,58 +40,61 @@ type World struct {
 	// Start of server time
 	start time.Time
 
-	// Registered players.
-	players map[*Player]bool
+	// Registered clients.
+	clients map[*Client]bool
 
-	// Input messages from the players.
+	// Input messages from the clients.
 	input chan []byte
 
 	// Outging updates from the server
 	update chan []byte
 
-	// Player join
-	join chan *Player
+	// Client join
+	join chan *Client
 
-	// Player leave
-	leave chan *Player
+	// Client leave
+	leave chan *Client
 }
 
 func makeWorld() *World {
 	return &World{
 		start: time.Now(),
-		players: make(map[*Player]bool),
+		clients: make(map[*Client]bool),
 		input: make(chan []byte),
 		update: make(chan []byte),
-		join: make(chan *Player),
-		leave: make(chan *Player),
+		join: make(chan *Client),
+		leave: make(chan *Client),
 	}
 }
 
 func (world *World) run_networking() {
 	for {
 		select {
-		case player := <-world.join:
-			world.players[player] = true
-		case player := <-world.leave:
-			if _, exists := world.players[player]; exists {
-				delete(world.players, player)
-				close(player.send)
-				player.conn.Close()
+		case client := <-world.join:
+			world.clients[client] = true
+		case client := <-world.leave:
+			if _, exists := world.clients[client]; exists {
+				delete(world.clients, client)
+				close(client.send)
+				client.conn.Close()
+				log.Print("client left")
 			}
 		case message := <-world.input:
-			// Handle player input here.
-			log.Print("player: ", message)
+			// Handle client input here.
+			status := &gp.Status{}
+			proto.Unmarshal(message, status)
+			log.Print("client: ", status)
 		case update := <-world.update:
 			msg, err := ws.NewPreparedMessage(ws.BinaryMessage, update)
 			if err != nil {
 				log.Print("prepare_error:", err)
 			}
-			for player := range world.players {
+			for client := range world.clients {
 				select {
-				case player.send <- msg:
+				case client.send <- msg:
 				default:
-					close(player.send)
-					delete(world.players, player)
+					close(client.send)
+					delete(world.clients, client)
 				}
 			}
 		}
@@ -90,23 +106,40 @@ func (world *World) run_simulation() {
 		now := time.Now()
 		server_time := now.Sub(world.start).Seconds()
 
+		fmt.Println(server_time)
 		test_renderable := &gp.Renderable{
 			Id: 1,
 			Color: 0,
-			Size: 1.0, 
+			Size: 0.4, 
 		}
 		test_entity := &gp.Entity{
 			Id: 23,
 			PosX: float32(math.Sin(server_time)),
-			PosY: float32(math.Cos(server_time)),
+			PosY: -0.4,//float32(math.Cos(server_time)),
+			VelX: float32(math.Cos(server_time)),
+			VelY: 0.0,//-float32(math.Sin(server_time)),
+			Renderable: []*gp.Renderable{test_renderable},
+		}
+		test_entity_2 := &gp.Entity{
+			Id: 24,
+			PosX: float32(math.Sin(server_time * 0.3)),
+			PosY: 0.4,//float32(math.Cos(server_time)),
+			VelX: float32(math.Cos(server_time * 0.3) / 3.0),
+			VelY: 0.0,//-float32(math.Sin(server_time)),
 			Renderable: []*gp.Renderable{test_renderable},
 		}
 		update := &gp.Update{
 			Time: server_time,
-			Entity: []*gp.Entity{test_entity},
+			Entity: []*gp.Entity{test_entity, test_entity_2},
 			Remove: []uint32{1, 2},
+			CamX: 0.0,
+			CamY: float32(math.Cos(server_time)),
+			CamScale: 2.0 + float32(math.Cos(server_time)),
 		}
-		out, _ := proto.Marshal(update)
+		out, err := proto.Marshal(update)
+		if err != nil {
+			log.Print(err)
+		}
 		packet := make([]byte, 2, len(out) + 2)
 		binary.LittleEndian.PutUint16(packet, uint16(len(out)))
 		packet = append(packet, out...)
@@ -123,46 +156,73 @@ var upgrader = ws.Upgrader{
 	Subprotocols: []string{"binary"},
 }
 
-func (p *Player) sendUpdate() {
+func (c *Client) readUpdate() {
 	defer func() {
-		p.conn.Close()
-		p.world.leave <- p
+		c.world.leave <- c
+		c.conn.Close()
+	}()
+
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil})
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if ws.IsUnexpectedCloseError(err, ws.CloseGoingAway, ws.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		c.world.input <- message
+	}
+}
+
+func (c *Client) sendUpdate() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
 	}()
 
 	for {
 		select {
-		case msg, ok := <-p.send:
+		case msg, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
 			
 			if !ok {
 				// The hub closed the channel.
-				p.conn.WriteMessage(ws.CloseMessage, []byte{})
+				c.conn.WriteMessage(ws.CloseMessage, []byte{})
 				return
 			}
 
-			err := p.conn.WritePreparedMessage(msg)
+			err := c.conn.WritePreparedMessage(msg)
 			if err != nil {
-				log.Print("error sending update: ", p)
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(ws.PingMessage, nil); err != nil {
+				return
 			}
 		}
 	}
 }
 
-func handlePlayer(world *World, w http.ResponseWriter, r *http.Request) {
+func handleClient(world *World, w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Print("upgrade error:", err)
 		return
 	}
 
-	player := &Player{
+	client := &Client{
 		world: world,
 		conn: conn,
 		send: make(chan *ws.PreparedMessage),
 	}
-	world.join <- player
+	world.join <- client
 
-	//go player.readInput()
-	go player.sendUpdate()
+	go client.readUpdate()
+	go client.sendUpdate()
 }
 
 func main() {
@@ -173,7 +233,7 @@ func main() {
 	go world.run_networking()
 	go world.run_simulation()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		handlePlayer(world, w, r)
+		handleClient(world, w, r)
 	})
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
